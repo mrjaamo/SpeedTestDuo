31635834b637f88fe6783b4c73b54a73df4ba7e1
mrjaamo <sonjaamo@gmail.com>
2022-02-14 14:12:40 +0200

    Simple demo game added

--- Assets/GameManagerUI.cs
+++ Assets/GameManagerUI.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManagerUI : MonoBehaviour
 {
-    public GameObject Player1;
-    public GameObject Player2;
+    public TMP_Text Player1Text;
+    public TMP_Text Player2Text;
 
-    public Text Player1Text;
-    public Text Player2Text;
-
+    string CurrentColor;
     public int color;
 
-    bool Player1Plays;
+    public int points = 0;
+
+    int clickedColor;
+
+    public bool Player1Plays = true;
     // Start is called before the first frame update
     void Start()
     {
-        Player1 = GameObject.Find("player1");
-        Player2 = GameObject.Find("player2");
+        Player1Text = GameObject.Find("Player1Info").GetComponent<TMP_Text>();
+        Player2Text = GameObject.Find("Player2Info").GetComponent<TMP_Text>();
+        SwitchSide();
     }
 
     // Update is called once per frame
@@ -26,53 +30,111 @@ public class GameManagerUI : MonoBehaviour
     {
 
 
-
     }
 
 
     void SwitchSide()
     {
-        if (Player1Plays == true) Player1Plays = false;
-        
+        if (Player1Plays == true)
+        {
+
+            Player1Plays = false;
+            color = Random.Range(6, 10);
+        }
+
         else
-        Player1Plays = true;
+        {
+            Player1Plays = true;
+            color = Random.Range(1, 5);
+        }
+
+        SetColor();
     }
 
-    void ShowColor()
+    void SetColor()
     {
         switch (color)
         {
             case 1:
                 // color red
-
-
-
+                CurrentColor = "Red";
                 break;
             case 2:
                 // color blue
-
+                CurrentColor = "Blue";
 
                 break;
             case 3:
                 // color yellow
-
+                CurrentColor = "Yellow";
 
 
                 break;
             case 4:
                 // color green
+                CurrentColor = "Green";
+                break;
+            case 6:
+                // color red
+                CurrentColor = "Red";
+                break;
+            case 7:
+                // color blue
+                CurrentColor = "Blue";
 
+                break;
+            case 8:
+                // color yellow
+                CurrentColor = "Yellow";
+
+
+                break;
+            case 9:
+                // color green
+                CurrentColor = "Green";
 
                 break;
             default:
                 // code block
-
-
+                CurrentColor = "";
                 break;
         }
+        ShowColor();
     }
 
+    public void ClickColor(int Clickedcolor)
+    {
+        if (Clickedcolor != color)
+        {
+            points--;
+        }
 
+        if (Clickedcolor == color)
+        {
+            points++;
+            SwitchSide();
+        }
+     
+    }
 
+    void ShowColor()
+    {
+        if (Player1Plays == true)
+        {
+            Player1Text.text = points.ToString();
+            Player2Text.text = CurrentColor;
+        }
+
+        if (Player1Plays == false)
+        {
+            Player1Text.text = CurrentColor;
+            Player2Text.text = points.ToString();
+
+        }
+    }
+    void addpoints()
+    {
+    
+    }
 
 }
